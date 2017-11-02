f799f923c1845549a462f9039405ed910ca42c17
lilinhao <lilinhao@meituan.com>
2017-11-02 11:00:55 +0800

    *

--- luckey.js
+++ luckey.js
@@ -22,6 +22,29 @@ const pick = (amount, n) => {
   const drawer = draw(amount);
   return new Array(n).fill().map((e, i) => drawer.next().value);
 } 
+console.log(pick(30, 5));
+console.log(pick(30, 5));
 
-console.log(pick(30, 5));
-console.log(pick(30, 5));
+/**
+  返回值期间 [0, 0.25)，概率 0.01
+  返回值期间 [0.25, 0.5)，概率 0.03
+  返回值期间 [0.5, 0.75)，概率 0.06
+  返回值期间 [0.75, 1)，概率 0.9
+ */
+function updateRandom() {
+  let p = Math.random(),
+    n = Math.random() / 4;
+  if (p < 0.01) {
+    return 0 + n;
+  }
+  if (p < 0.04) {
+    return 0.25 + n;
+  }
+  if (p < 0.1) {
+    return 0.5 + n;
+  }
+  if (p < 1) {
+    return 0.75 + n;
+  }
+}
+console.log(updateRandom());
